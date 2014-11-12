c2e690ee25f2b53cd09075cef2a27d9f93f767c0
CrzyRndm <joshcrawfy@gmail.com>
2014-11-12 22:58:33 +1300

    wing leveller works

---
+++ AutoPilot/PID_Controller.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using UnityEngine;
+
+namespace PilotAid.PID
+{
+    public class PID_Controller : MonoBehaviour
+    {
+        private double setpoint = 0; // process setpoint
+
+        private double k_proportional = 0; // Kp
+        private double k_integral = 0; // Ki
+        private double k_derivative = 0; // Kd
+
+        private double sum = 0; // integral sum
+        private double previous = 0; // previous value stored for derivative action
+        private double rolling_diff = 0; // used for rolling average difference
+        private double rollingFactor = 0; // rolling average proportion. 0 = all new, 1 = never changes
+        private double error = 0; // error of current iteration
+
+        private double inMin = -180; // Minimum input value
+        private double inMax = 180; // Maximum input value
+
+        private double outMin = -1; // Minimum output value
+        private double outMax = 1; // Maximum output value
+
+        private double integralClampUpper = 1; // AIW clamp
+        private double integralClampLower = -1; // AIW clamp
+
+        private double dt = 1; // standardised response for any physics dt setting. Check if this can be changed after module initialisation (ie. in flight)
+
+        public PID_Controller(double Kp, double Ki, double Kd)
+        {
+            k_proportional = Kp;
+            k_integral = Ki;
+            k_derivative = Kd;
+        }
+
+        public double Response(double input)
+        {
+            input = Clamp(input, inMin, inMax);
+            print("input: " + input.ToString());
+            dt = TimeWarp.fixedDeltaTime;
+            print("dt: " + dt.ToString());
+            error = input - setpoint;
+            print("error" + error.ToString());
+            return Clamp(proportionalError(input) + integralError(input) + derivativeError(input), outMin, outMax);
+        }
+
+        private double proportionalError(double input)
+        {
+            if (k_proportional == 0)
+                return 0;
+            return error * k_proportional;
+        }
+
+        private double integralError(double input)
+        {
+            if (k_integral == 0)
+                return 0;
+
+            sum += error * dt * k_integral;
+            Clamp(sum, integralClampLower, integralClampUpper); // AIW
+
+            return sum;
+        }
+
+        private double derivativeError(double input)
+        {
+            if (k_derivative == 0)
+                return 0;
+
+            double difference = (input - previous) / dt;
+            rolling_diff = rolling_diff * rollingFactor + difference * (1 - rollingFactor); // rolling average might help smooth out jumpy derivative response
+            
+            previous = input;
+            return rolling_diff * k_derivative;
+        }
+
+        #region utility functions
+
+        /// <summary>
+        /// Clamp double input between maximum and minimum value
+        /// </summary>
+        /// <param name="val">variable to be clamped</param>
+        /// <param name="min">minimum output value of the variable</param>
+        /// <param name="max">maximum output value of the variable</param>
+        /// <returns>val clamped between max and min</returns>
+        internal static double Clamp(double val, double min, double max)
+        {
+            if (val < min)
+                return min;
+            else if (val > max)
+                return max;
+            else
+                return val;
+        }
+
+        /// <summary>
+        /// Linear interpolation between two points
+        /// </summary>
+        /// <param name="pct">fraction of travel from the minimum to maximum. Can be less than 0 or greater than 1</param>
+        /// <param name="lower">reference point treated as the base (pct = 0)</param>
+        /// <param name="upper">reference point treated as the target (pct = 1)</param>
+        /// <param name="clamp">clamp pct input between 0 and 1?</param>
+        /// <returns></returns>
+        internal static double Lerp(double pct, double lower, double upper, bool clamp = true)
+        {
+            if (clamp)
+            {
+                pct = Clamp(pct, 0, 1);
+            }
+            return (1 - pct) * lower + pct * upper;
+        }
+
+        #endregion
+
+        #region properties
+        public double SetPoint
+        {
+            set
+            {
+                setpoint = value;
+            }
+        }
+
+        public double PGain
+        {
+            get
+            {
+                return k_proportional;
+            }
+            set
+            {
+                k_proportional = value;
+            }
+        }
+
+        public double IGain
+        {
+            get
+            {
+                return k_integral;
+            }
+            set
+            {
+                k_integral = value;
+            }
+        }
+
+        public double DGain
+        {
+            get
+            {
+                return k_derivative;
+            }
+            set
+            {
+                k_derivative = value;
+            }
+        }
+
+        public double InMin
+        {
+            set
+            {
+                inMin = value;
+            }
+        }
+
+        public double InMax
+        {
+            set
+            {
+                inMax = value;
+            }
+        }
+
+        public double OutMin
+        {
+            set
+            {
+                outMin = value;
+            }
+        }
+
+        public double OutMax
+        {
+            set
+            {
+                outMax = value;
+            }
+        }
+
+        public double ClampLower
+        {
+            set
+            {
+                integralClampLower = value;
+            }
+        }
+
+        public double ClampUpper
+        {
+            set
+            {
+                integralClampUpper = value;
+            }
+        }
+
+        public double RollingFactor
+        {
+            set
+            {
+                rollingFactor = Clamp(value, 0, 1);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ AutoPilot/PilotAid.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using UnityEngine;
+
+namespace PilotAid
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    class PilotAid : MonoBehaviour
+    {        
+        Vessel thisVessel = null;
+        PID.PID_Controller wingsLevel = new PID.PID_Controller(0.01, 0.01, 0.01);
+
+        Rect window = new Rect(Screen.width - 400, 40, 200, 400);
+        bool button = false;
+
+        public void Start()
+        {
+            thisVessel = FlightGlobals.ActiveVessel;
+            thisVessel.OnFlyByWire += new FlightInputCallback(vesselController);
+        }
+
+        public void Update()
+        {
+            /*
+            print("x: " + thisVessel.ReferenceTransform.eulerAngles.x.ToString()); // roll
+            print("y: " + thisVessel.ReferenceTransform.eulerAngles.y.ToString()); // pitch
+            print("z: " + thisVessel.ReferenceTransform.eulerAngles.z.ToString()); // heading / yaw
+            */
+        }
+
+        public void FixedUpdate()
+        {
+        }
+
+        public void OnGUI()
+        {
+            if (!bDisplay)
+                return;
+
+            window = GUI.Window(34244, window, displayWindow, "");
+        }
+
+        private void displayWindow(int id)
+        {
+            GUILayout.BeginVertical();
+
+            GUILayout.Label("Wing Leveller");
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Kp: ", GUILayout.Width(50));
+            string text = GUILayout.TextField(wingsLevel.PGain.ToString());
+            wingsLevel.PGain = double.Parse(text);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Ki: ", GUILayout.Width(50));
+            text = GUILayout.TextField(wingsLevel.IGain.ToString());
+            wingsLevel.IGain = double.Parse(text);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Kd: ", GUILayout.Width(50));
+            text = GUILayout.TextField(wingsLevel.DGain.ToString());
+            wingsLevel.DGain = double.Parse(text);
+            GUILayout.EndHorizontal();
+
+            button = GUILayout.Toggle(button, "Active");
+
+            GUILayout.EndVertical();
+        }
+
+        private NavBall ball;
+
+        private void vesselController(FlightCtrlState state)
+        {
+            if (!HighLogic.LoadedSceneIsFlight)
+                return;
+            if (thisVessel == null)
+                return;
+            if (!button)
+                return;
+
+            // blatant copying of FAR get attitude logic because its just so straightfoward...
+            if (ball == null)
+                ball = FlightUIController.fetch.GetComponentInChildren<NavBall>();
+
+            Quaternion vesselRot = Quaternion.Inverse(ball.relativeGymbal);
+            double pitch = (vesselRot.eulerAngles.x > 180) ? (vesselRot.eulerAngles.x - 360) : vesselRot.eulerAngles.x;
+            double roll = (vesselRot.eulerAngles.z > 180) ? (vesselRot.eulerAngles.z - 360) : vesselRot.eulerAngles.z;
+            double heading = vesselRot.eulerAngles.y;
+
+            Vector3 tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + thisVessel.ReferenceTransform.forward * Vector3.Dot(thisVessel.ReferenceTransform.forward, thisVessel.srf_velocity.normalized);   //velocity vector projected onto a plane that divides the airplane into left and right halves
+            double AoA = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.forward);
+            AoA = 180 / Math.PI * Math.Asin(AoA);
+            if (double.IsNaN(AoA))
+                AoA = 0;
+
+            tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + thisVessel.ReferenceTransform.right * Vector3.Dot(thisVessel.ReferenceTransform.right, thisVessel.srf_velocity.normalized);     //velocity vector projected onto the vehicle-horizontal plane
+            double yaw = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.right);
+            yaw = 180 / Math.PI * Math.Asin(yaw);
+            if (double.IsNaN(yaw))
+                yaw = 0;
+
+
+            // Functionality here
+            print("roll: " + roll.ToString());
+            state.roll = (float)PID.PID_Controller.Clamp(wingsLevel.Response(roll) + state.roll, -1, 1);
+        }
+
+
+        #region Applauncher Functions and Variables
+
+        private ApplicationLauncherButton btnLauncher;
+        private bool bDisplay = false;
+
+        private void Awake()
+        {
+            GameEvents.onGUIApplicationLauncherReady.Add(this.OnAppLauncherReady);
+        }
+
+        private void OnDestroy()
+        {
+            GameEvents.onGUIApplicationLauncherReady.Remove(this.OnAppLauncherReady);
+            if (btnLauncher != null)
+                ApplicationLauncher.Instance.RemoveModApplication(this.btnLauncher);
+        }
+
+        private void OnAppLauncherReady()
+        {
+            btnLauncher = ApplicationLauncher.Instance.AddModApplication(OnToggleTrue, OnToggleFalse,
+                                                                        null, null, null, null,
+                                                                        ApplicationLauncher.AppScenes.ALWAYS,
+                                                                        GameDatabase.Instance.GetTexture("FlightAids/Icons/AppLauncherIcon", false));
+        }
+
+        private void OnGameSceneChange(GameScenes scene)
+        {
+            bDisplay = false;
+            ApplicationLauncher.Instance.RemoveModApplication(btnLauncher);
+        }
+
+        private void OnToggleTrue()
+        {
+            bDisplay = true;
+        }
+
+        private void OnToggleFalse()
+        {
+            bDisplay = false;
+        }
+        #endregion
+    }
+}
