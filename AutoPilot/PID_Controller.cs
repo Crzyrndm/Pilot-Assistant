a552a6e1f60c74aead33cdebe15aa0b3633c69d6
CrzyRndm <joshcrawfy@gmail.com>
2014-11-14 15:26:20 +1300

    Altitude and climb AP completed

--- AutoPilot/PID_Controller.cs
+++ AutoPilot/PID_Controller.cs
@@ -7,9 +7,9 @@ namespace PilotAid.PID
     {
         private double setpoint = 0; // process setpoint
 
-        private double k_proportional = 0; // Kp
-        private double k_integral = 0; // Ki
-        private double k_derivative = 0; // Kd
+        private double k_proportional; // Kp
+        private double k_integral; // Ki
+        private double k_derivative; // Kd
 
         private double sum = 0; // integral sum
         private double previous = 0; // previous value stored for derivative action
@@ -17,32 +17,33 @@ namespace PilotAid.PID
         private double rollingFactor = 0; // rolling average proportion. 0 = all new, 1 = never changes
         private double error = 0; // error of current iteration
 
-        private double inMin = -180; // Minimum input value
-        private double inMax = 180; // Maximum input value
+        private double inMin = -1000000000; // Minimum input value
+        private double inMax = 1000000000; // Maximum input value
 
-        private double outMin = -1; // Minimum output value
-        private double outMax = 1; // Maximum output value
+        private double outMin; // Minimum output value
+        private double outMax; // Maximum output value
 
-        private double integralClampUpper = 1; // AIW clamp
-        private double integralClampLower = -1; // AIW clamp
+        private double integralClampUpper; // AIW clamp
+        private double integralClampLower; // AIW clamp
 
         private double dt = 1; // standardised response for any physics dt setting. Check if this can be changed after module initialisation (ie. in flight)
 
-        public PID_Controller(double Kp, double Ki, double Kd)
+        public PID_Controller(double Kp, double Ki, double Kd, double OutputMin, double OutputMax, double intClampLower, double intClampUpper)
         {
             k_proportional = Kp;
             k_integral = Ki;
             k_derivative = Kd;
+            outMin = OutputMin;
+            outMax = OutputMax;
+            integralClampLower = intClampLower;
+            integralClampUpper = intClampUpper;
         }
 
         public double Response(double input)
         {
             input = Clamp(input, inMin, inMax);
-            print("input: " + input.ToString());
             dt = TimeWarp.fixedDeltaTime;
-            print("dt: " + dt.ToString());
             error = input - setpoint;
-            print("error" + error.ToString());
             return Clamp(proportionalError(input) + integralError(input) + derivativeError(input), outMin, outMax);
         }
 
@@ -56,7 +57,10 @@ namespace PilotAid.PID
         private double integralError(double input)
         {
             if (k_integral == 0)
-                return 0;
+            {
+                sum = 0;
+                return sum;
+            }
 
             sum += error * dt * k_integral;
             Clamp(sum, integralClampLower, integralClampUpper); // AIW
@@ -74,6 +78,11 @@ namespace PilotAid.PID
             
             previous = input;
             return rolling_diff * k_derivative;
+        }
+
+        public void Clear()
+        {
+            sum = 0;
         }
 
         #region utility functions
@@ -117,6 +126,10 @@ namespace PilotAid.PID
         #region properties
         public double SetPoint
         {
+            get
+            {
+                return setpoint;
+            }
             set
             {
                 setpoint = value;
@@ -175,16 +188,30 @@ namespace PilotAid.PID
             }
         }
 
+        /// <summary>
+        /// Set output minimum and anti-integral windup to value
+        /// </summary>
         public double OutMin
         {
+            get
+            {
+                return outMin;
+            }
             set
             {
                 outMin = value;
             }
         }
 
+        /// <summary>
+        /// Set output maximum and anti-integral windup to value
+        /// </summary>
         public double OutMax
         {
+            get
+            {
+                return outMax;
+            }
             set
             {
                 outMax = value;
@@ -193,6 +220,10 @@ namespace PilotAid.PID
 
         public double ClampLower
         {
+            get
+            {
+                return integralClampLower;
+            }
             set
             {
                 integralClampLower = value;
@@ -201,6 +232,10 @@ namespace PilotAid.PID
 
         public double ClampUpper
         {
+            get
+            {
+                return integralClampUpper;
+            }
             set
             {
                 integralClampUpper = value;
--- AutoPilot/PilotAid.cs
+++ AutoPilot/PilotAid.cs
@@ -6,25 +6,43 @@ namespace PilotAid
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     class PilotAid : MonoBehaviour
     {        
-        Vessel thisVessel = null;
-        PID.PID_Controller wingsLevel = new PID.PID_Controller(0.01, 0.01, 0.01);
+        private Vessel thisVessel = null;
+        private PID.PID_Controller HeadingController = new PID.PID_Controller(0.01, 0, 0, -30, 30, 0, 0);
+        private PID.PID_Controller RollController = new PID.PID_Controller(0.01, 0.01, 0.01, -1, 1, -0.1, 0.1);
 
-        Rect window = new Rect(Screen.width - 400, 40, 200, 400);
-        bool button = false;
+        private PID.PID_Controller AltitudeToClimbRate = new PID.PID_Controller(0.1, 0, 0, -20, 20, -1, 1); // P control for converting altitude hold to climb rate
+        private PID.PID_Controller PitchController = new PID.PID_Controller(1, 0.4, 0.6, -10, 10, -2, 2); // Input craft altitude, output target craft pitch
+        private PID.PID_Controller ElevatorController = new PID.PID_Controller(0.01, 0.01, 0.01, -1, 1, -0.1, 0.1); // Convert pitch input to control surface deflection
+
+        private PID.PID_Controller YawController = new PID.PID_Controller(0.01, 0.01, 0.01, 0, 0, 0, 0);
+
+        private Rect window = new Rect(200, 40, 1500, 500);
+        // RollController
+        private bool rollActive = false;
+        // PitchController
+        private bool pitchActive = false;
+        private bool pitchWasActive = false;
+        private bool bAltitudeHold = true;
+
+        private double pitch = 0, roll = 0, yaw = 0, AoA = 0, heading = 0; // currenct craft attitude variables
+
+        private string targetAltitude = "0";
+        private string targetSpeed = "0";
+
 
         public void Start()
         {
             thisVessel = FlightGlobals.ActiveVessel;
             thisVessel.OnFlyByWire += new FlightInputCallback(vesselController);
+
+            RollController.InMax = 180;
+            RollController.InMin = -180;
+
+            AltitudeToClimbRate.InMin = 0;
         }
 
         public void Update()
         {
-            /*
-            print("x: " + thisVessel.ReferenceTransform.eulerAngles.x.ToString()); // roll
-            print("y: " + thisVessel.ReferenceTransform.eulerAngles.y.ToString()); // pitch
-            print("z: " + thisVessel.ReferenceTransform.eulerAngles.z.ToString()); // heading / yaw
-            */
         }
 
         public void FixedUpdate()
@@ -42,33 +60,254 @@ namespace PilotAid
         private void displayWindow(int id)
         {
             GUILayout.BeginVertical();
+            
 
+            // Roll Control
             GUILayout.Label("Wing Leveller");
+            GUILayout.Label("Target Angle: " + RollController.SetPoint.ToString("N3") + "\u00B0");
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Kp: ", GUILayout.Width(50));
-            string text = GUILayout.TextField(wingsLevel.PGain.ToString());
-            wingsLevel.PGain = double.Parse(text);
+            GUILayout.Label("Kp: ", GUILayout.Width(80));
+            string text = GUILayout.TextField(RollController.PGain.ToString());
+            RollController.PGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Ki: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.IGain.ToString());
+            RollController.IGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Kd: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.DGain.ToString());
+            RollController.DGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Min Out: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.OutMin.ToString());
+            RollController.OutMin = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Max Out: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.OutMax.ToString());
+            RollController.OutMax = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Clamp Lower: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.ClampLower.ToString());
+            RollController.ClampLower = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Clamp Upper: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.ClampUpper.ToString());
+            RollController.ClampUpper = double.Parse(text);
+            GUILayout.EndHorizontal();
+
+            rollActive = GUILayout.Toggle(rollActive, "Wing Leveller is Active?");
+
+
+            //// Pitch Controls
+            GUILayout.Label("", GUILayout.Height(30)); // vertical spacer
+            GUILayout.Label("Altitude Hold");
+            
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Target Altitude: ", GUILayout.Width(100));
+            //
+            targetAltitude = GUILayout.TextField(targetAltitude, GUILayout.Width(100));
+            if (GUILayout.Button("Update Target Altitude", GUILayout.Width(150)))
+            {
+                double newAlt;
+                double.TryParse(targetAltitude, out newAlt);
+                if (newAlt > 0)
+                {
+                    AltitudeToClimbRate.SetPoint = newAlt;
+                    bAltitudeHold = true;
+                }
+            }
+            //
+            GUILayout.Space(50);
+            GUILayout.Label("Current Altitude: " + thisVessel.altitude.ToString("N1") + "m", GUILayout.Width(250));
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Ki: ", GUILayout.Width(50));
-            text = GUILayout.TextField(wingsLevel.IGain.ToString());
-            wingsLevel.IGain = double.Parse(text);
+            GUILayout.Label("Target Speed: ", GUILayout.Width(100));
+            //
+            targetSpeed = GUILayout.TextField(targetSpeed, GUILayout.Width(100));
+            if (GUILayout.Button("Update Target Speed", GUILayout.Width(150)))
+            {
+                double newSpeed;
+                double.TryParse(targetSpeed, out newSpeed);
+                print(newSpeed);
+                if (newSpeed != 0)
+                {
+                    PitchController.SetPoint = newSpeed;
+                    bAltitudeHold = false;
+                }
+            }
+            //
+            GUILayout.Space(50);
+            GUILayout.Label("Current Speed: " + thisVessel.verticalSpeed.ToString("N3") + "m/s", GUILayout.Width(250));
+            GUILayout.EndHorizontal();
+
+            bAltitudeHold = GUILayout.Toggle(bAltitudeHold, "Altitude Hold?", GUILayout.Width(200));
+
+            if (bAltitudeHold)
+            {
+                GUILayout.BeginHorizontal();
+
+                GUILayout.Label("Altitude Kp: ", GUILayout.Width(80));
+                text = GUILayout.TextField(AltitudeToClimbRate.PGain.ToString(), GUILayout.Width(40));
+                AltitudeToClimbRate.PGain = double.Parse(text);
+
+                GUILayout.Space(20);
+
+                GUILayout.Label("Altitude Ki: ", GUILayout.Width(80));
+                text = GUILayout.TextField(AltitudeToClimbRate.IGain.ToString(), GUILayout.Width(40));
+                AltitudeToClimbRate.IGain = double.Parse(text);
+
+                GUILayout.Space(20);
+
+                GUILayout.Label("Altitude Kd: ", GUILayout.Width(80));
+                text = GUILayout.TextField(AltitudeToClimbRate.DGain.ToString(), GUILayout.Width(40));
+                AltitudeToClimbRate.DGain = double.Parse(text);
+
+                GUILayout.Space(20);
+
+                GUILayout.Label("Altitude Min Out: ", GUILayout.Width(80));
+                text = GUILayout.TextField(AltitudeToClimbRate.OutMin.ToString(), GUILayout.Width(40));
+                AltitudeToClimbRate.OutMin = double.Parse(text);
+
+                GUILayout.Space(20);
+
+                GUILayout.Label("Altitude Max Out: ", GUILayout.Width(80));
+                text = GUILayout.TextField(AltitudeToClimbRate.OutMax.ToString(), GUILayout.Width(40));
+                AltitudeToClimbRate.OutMax = double.Parse(text);
+
+                GUILayout.Space(20);
+
+                GUILayout.Label("Clamp Min: ", GUILayout.Width(80));
+                text = GUILayout.TextField(AltitudeToClimbRate.ClampLower.ToString(), GUILayout.Width(40));
+                AltitudeToClimbRate.ClampLower = double.Parse(text);
+
+                GUILayout.Space(20);
+
+                GUILayout.Label("Clamp Max: ", GUILayout.Width(80));
+                text = GUILayout.TextField(AltitudeToClimbRate.ClampUpper.ToString(), GUILayout.Width(40));
+                AltitudeToClimbRate.ClampUpper = double.Parse(text);
+
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Pitch Kp: ", GUILayout.Width(80));
+            text = GUILayout.TextField(PitchController.PGain.ToString());
+            PitchController.PGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Pitch Ki: ", GUILayout.Width(80));
+            text = GUILayout.TextField(PitchController.IGain.ToString());
+            PitchController.IGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Pitch Kd: ", GUILayout.Width(80));
+            text = GUILayout.TextField(PitchController.DGain.ToString());
+            PitchController.DGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Pitch Min Out: ", GUILayout.Width(80));
+            text = GUILayout.TextField(PitchController.OutMin.ToString());
+            PitchController.OutMin = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Pitch Max Out: ", GUILayout.Width(80));
+            text = GUILayout.TextField(PitchController.OutMax.ToString());
+            PitchController.OutMax = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Pitch Clamp Min: ", GUILayout.Width(80));
+            text = GUILayout.TextField(PitchController.ClampLower.ToString());
+            PitchController.ClampLower = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Pitch Clamp Max: ", GUILayout.Width(80));
+            text = GUILayout.TextField(PitchController.ClampUpper.ToString());
+            PitchController.ClampUpper = double.Parse(text);
+
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Kd: ", GUILayout.Width(50));
-            text = GUILayout.TextField(wingsLevel.DGain.ToString());
-            wingsLevel.DGain = double.Parse(text);
+
+            GUILayout.Label("Elevator Kp: ", GUILayout.Width(80));
+            text = GUILayout.TextField(ElevatorController.PGain.ToString());
+            ElevatorController.PGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Elevator Ki: ", GUILayout.Width(80));
+            text = GUILayout.TextField(ElevatorController.IGain.ToString());
+            ElevatorController.IGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Elevator Kd: ", GUILayout.Width(80));
+            text = GUILayout.TextField(ElevatorController.DGain.ToString());
+            ElevatorController.DGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Elevator Min Out: ", GUILayout.Width(80));
+            text = GUILayout.TextField(ElevatorController.OutMin.ToString());
+            ElevatorController.OutMin = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Elevator Max Out: ", GUILayout.Width(80));
+            text = GUILayout.TextField(ElevatorController.OutMax.ToString());
+            ElevatorController.OutMax = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Elevator Clamp Min: ", GUILayout.Width(80));
+            text = GUILayout.TextField(ElevatorController.ClampLower.ToString());
+            ElevatorController.ClampLower = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Elevator Clamp Max ", GUILayout.Width(80));
+            text = GUILayout.TextField(ElevatorController.ClampUpper.ToString());
+            ElevatorController.ClampUpper = double.Parse(text);
+
             GUILayout.EndHorizontal();
 
-            button = GUILayout.Toggle(button, "Active");
+            pitchActive = GUILayout.Toggle(pitchActive, "Active");
+            if (pitchActive && !pitchWasActive)
+            {
+                if (bAltitudeHold)
+                    targetAltitude = thisVessel.altitude.ToString();
+                AltitudeToClimbRate.SetPoint = thisVessel.altitude;
+                pitchWasActive = true;
+            }
+            if (!pitchActive && pitchWasActive)
+            {
+                AltitudeToClimbRate.Clear();
+                PitchController.Clear();
+                ElevatorController.Clear();
+                pitchWasActive = false;
+            }
 
             GUILayout.EndVertical();
         }
-
-        private NavBall ball;
 
         private void vesselController(FlightCtrlState state)
         {
@@ -76,36 +315,55 @@ namespace PilotAid
                 return;
             if (thisVessel == null)
                 return;
-            if (!button)
-                return;
 
+            updateAttitude();
+            
+            // Wing leveller
+            if (rollActive)
+                state.roll = (float)PID.PID_Controller.Clamp(RollController.Response(roll) + state.roll, -1, 1);
+
+            // Pitch Controller
+            // Work on vertical speed, altitude hold can use a proportional error as input
+            // meh - look into switching between vert speed and altitude control. Alt hold with vert speed is pathetic
+            if (pitchActive)
+            {
+                // Set requested vertical speed
+                if (bAltitudeHold)
+                    PitchController.SetPoint = -AltitudeToClimbRate.Response(thisVessel.altitude);
+
+                ElevatorController.SetPoint = -PitchController.Response(thisVessel.verticalSpeed);
+                state.pitch = (float)-ElevatorController.Response(pitch);
+            }
+        }
+
+
+        private NavBall ball;
+        private void updateAttitude()
+        {
             // blatant copying of FAR get attitude logic because its just so straightfoward...
             if (ball == null)
                 ball = FlightUIController.fetch.GetComponentInChildren<NavBall>();
 
+            // pitch/roll/heading
             Quaternion vesselRot = Quaternion.Inverse(ball.relativeGymbal);
-            double pitch = (vesselRot.eulerAngles.x > 180) ? (vesselRot.eulerAngles.x - 360) : vesselRot.eulerAngles.x;
-            double roll = (vesselRot.eulerAngles.z > 180) ? (vesselRot.eulerAngles.z - 360) : vesselRot.eulerAngles.z;
-            double heading = vesselRot.eulerAngles.y;
+            pitch = (vesselRot.eulerAngles.x > 180) ? (360 - vesselRot.eulerAngles.x) : -vesselRot.eulerAngles.x; // pitch up is +ve
+            roll = (vesselRot.eulerAngles.z > 180) ? (vesselRot.eulerAngles.z - 360) : vesselRot.eulerAngles.z;
+            heading = vesselRot.eulerAngles.y;
 
+            // AoA
             Vector3 tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + thisVessel.ReferenceTransform.forward * Vector3.Dot(thisVessel.ReferenceTransform.forward, thisVessel.srf_velocity.normalized);   //velocity vector projected onto a plane that divides the airplane into left and right halves
-            double AoA = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.forward);
+            AoA = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.forward);
             AoA = 180 / Math.PI * Math.Asin(AoA);
             if (double.IsNaN(AoA))
                 AoA = 0;
 
+            // yaw? horizontal AoA?
             tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + thisVessel.ReferenceTransform.right * Vector3.Dot(thisVessel.ReferenceTransform.right, thisVessel.srf_velocity.normalized);     //velocity vector projected onto the vehicle-horizontal plane
-            double yaw = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.right);
+            yaw = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.right);
             yaw = 180 / Math.PI * Math.Asin(yaw);
             if (double.IsNaN(yaw))
                 yaw = 0;
-
-
-            // Functionality here
-            print("roll: " + roll.ToString());
-            state.roll = (float)PID.PID_Controller.Clamp(wingsLevel.Response(roll) + state.roll, -1, 1);
         }
-
 
         #region Applauncher Functions and Variables
 
