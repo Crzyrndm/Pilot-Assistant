74fd11fa4b532bacb74038df73b275d0893db60e
CrzyRndm <joshcrawfy@gmail.com>
2014-11-14 16:23:16 +1300

    Heading Control

--- AutoPilot/PilotAid.cs
+++ AutoPilot/PilotAid.cs
@@ -7,11 +7,11 @@ namespace PilotAid
     class PilotAid : MonoBehaviour
     {        
         private Vessel thisVessel = null;
-        private PID.PID_Controller HeadingController = new PID.PID_Controller(0.01, 0, 0, -30, 30, 0, 0);
+        private PID.PID_Controller HeadingController = new PID.PID_Controller(1, 0.1, 0, -30, 30, -0.1, 0.1);
         private PID.PID_Controller RollController = new PID.PID_Controller(0.01, 0.01, 0.01, -1, 1, -0.1, 0.1);
 
-        private PID.PID_Controller AltitudeToClimbRate = new PID.PID_Controller(0.1, 0, 0, -20, 20, -1, 1); // P control for converting altitude hold to climb rate
-        private PID.PID_Controller PitchController = new PID.PID_Controller(1, 0.4, 0.6, -10, 10, -2, 2); // Input craft altitude, output target craft pitch
+        private PID.PID_Controller AltitudeToClimbRate = new PID.PID_Controller(0.1, 0, 0, -30, 30, -1, 1); // P control for converting altitude hold to climb rate
+        private PID.PID_Controller PitchController = new PID.PID_Controller(3, 0.4, 1.5, -10, 10, -2, 2); // Input craft altitude, output target craft pitch
         private PID.PID_Controller ElevatorController = new PID.PID_Controller(0.01, 0.01, 0.01, -1, 1, -0.1, 0.1); // Convert pitch input to control surface deflection
 
         private PID.PID_Controller YawController = new PID.PID_Controller(0.01, 0.01, 0.01, 0, 0, 0, 0);
@@ -28,6 +28,7 @@ namespace PilotAid
 
         private string targetAltitude = "0";
         private string targetSpeed = "0";
+        private string targetHeading = "0";
 
 
         public void Start()
@@ -63,48 +64,113 @@ namespace PilotAid
             
 
             // Roll Control
-            GUILayout.Label("Wing Leveller");
-            GUILayout.Label("Target Angle: " + RollController.SetPoint.ToString("N3") + "\u00B0");
+            GUILayout.Label("Heading Hold");
+
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Kp: ", GUILayout.Width(80));
-            string text = GUILayout.TextField(RollController.PGain.ToString());
+            GUILayout.Label("Target Heading: ", GUILayout.Width(100));
+            //
+            targetHeading = GUILayout.TextField(targetHeading, GUILayout.Width(100));
+            if (GUILayout.Button("Update Target Heading", GUILayout.Width(150)))
+            {
+                double newHdg;
+                double.TryParse(targetHeading, out newHdg);
+                if (newHdg >= 0 && newHdg <= 360)
+                {
+                    HeadingController.SetPoint = newHdg;
+                    rollActive = true;
+                }
+            }
+            //
+            GUILayout.Space(20);
+            GUILayout.Label("Current Heading: " + heading.ToString("N2") + "\u00B0", GUILayout.Width(250));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label("Heading Kp: ", GUILayout.Width(80));
+            string text = GUILayout.TextField(HeadingController.PGain.ToString(), GUILayout.Width(60));
+            HeadingController.PGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Heading Ki: ", GUILayout.Width(80));
+            text = GUILayout.TextField(HeadingController.IGain.ToString(), GUILayout.Width(60));
+            HeadingController.IGain = double.Parse(text);
+
+            GUILayout.Space(20);
+
+            GUILayout.Label("Heading Kd: ", GUILayout.Width(80));
+            text = GUILayout.TextField(HeadingController.DGain.ToString(), GUILayout.Width(60));
+            HeadingController.DGain = double.Parse(text);
+
+            GUILayout.Space(5);
+
+            GUILayout.Label("Heading Min Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(HeadingController.OutMin.ToString(), GUILayout.Width(60));
+            HeadingController.OutMin = double.Parse(text);
+
+            GUILayout.Space(5);
+
+            GUILayout.Label("Heading Max Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(HeadingController.OutMax.ToString(), GUILayout.Width(60));
+            HeadingController.OutMax = double.Parse(text);
+
+            GUILayout.Space(5);
+
+            GUILayout.Label("Heading Clamp Lower: ", GUILayout.Width(120));
+            text = GUILayout.TextField(HeadingController.ClampLower.ToString(), GUILayout.Width(60));
+            HeadingController.ClampLower = double.Parse(text);
+
+            GUILayout.Space(5);
+
+            GUILayout.Label("Heading Clamp Upper: ", GUILayout.Width(120));
+            text = GUILayout.TextField(HeadingController.ClampUpper.ToString(), GUILayout.Width(60));
+            HeadingController.ClampUpper = double.Parse(text);
+            
+            GUILayout.EndHorizontal();
+
+            GUILayout.Label("Target Angle: " + RollController.SetPoint.ToString("N3") + "\u00B0", GUILayout.Width(250));
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Roll Kp: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.PGain.ToString(), GUILayout.Width(60));
             RollController.PGain = double.Parse(text);
 
             GUILayout.Space(20);
 
-            GUILayout.Label("Ki: ", GUILayout.Width(80));
-            text = GUILayout.TextField(RollController.IGain.ToString());
+            GUILayout.Label("Roll Ki: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.IGain.ToString(), GUILayout.Width(60));
             RollController.IGain = double.Parse(text);
 
             GUILayout.Space(20);
 
-            GUILayout.Label("Kd: ", GUILayout.Width(80));
-            text = GUILayout.TextField(RollController.DGain.ToString());
+            GUILayout.Label("Roll Kd: ", GUILayout.Width(80));
+            text = GUILayout.TextField(RollController.DGain.ToString(), GUILayout.Width(60));
             RollController.DGain = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Min Out: ", GUILayout.Width(80));
-            text = GUILayout.TextField(RollController.OutMin.ToString());
+            GUILayout.Label("Roll Min Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(RollController.OutMin.ToString(), GUILayout.Width(60));
             RollController.OutMin = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Max Out: ", GUILayout.Width(80));
-            text = GUILayout.TextField(RollController.OutMax.ToString());
+            GUILayout.Label("Roll Max Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(RollController.OutMax.ToString(), GUILayout.Width(60));
             RollController.OutMax = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Clamp Lower: ", GUILayout.Width(80));
-            text = GUILayout.TextField(RollController.ClampLower.ToString());
+            GUILayout.Label("Roll Clamp Lower: ", GUILayout.Width(120));
+            text = GUILayout.TextField(RollController.ClampLower.ToString(), GUILayout.Width(60));
             RollController.ClampLower = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Clamp Upper: ", GUILayout.Width(80));
-            text = GUILayout.TextField(RollController.ClampUpper.ToString());
+            GUILayout.Label("Roll Clamp Upper: ", GUILayout.Width(120));
+            text = GUILayout.TextField(RollController.ClampUpper.ToString(), GUILayout.Width(60));
             RollController.ClampUpper = double.Parse(text);
             GUILayout.EndHorizontal();
 
@@ -112,7 +178,7 @@ namespace PilotAid
 
 
             //// Pitch Controls
-            GUILayout.Label("", GUILayout.Height(30)); // vertical spacer
+            GUILayout.Label("", GUILayout.Height(10)); // vertical spacer
             GUILayout.Label("Altitude Hold");
             
             GUILayout.BeginHorizontal();
@@ -127,10 +193,11 @@ namespace PilotAid
                 {
                     AltitudeToClimbRate.SetPoint = newAlt;
                     bAltitudeHold = true;
+                    pitchActive = true;
                 }
             }
             //
-            GUILayout.Space(50);
+            GUILayout.Space(20);
             GUILayout.Label("Current Altitude: " + thisVessel.altitude.ToString("N1") + "m", GUILayout.Width(250));
             GUILayout.EndHorizontal();
 
@@ -147,10 +214,11 @@ namespace PilotAid
                 {
                     PitchController.SetPoint = newSpeed;
                     bAltitudeHold = false;
+                    pitchActive = true;
                 }
             }
             //
-            GUILayout.Space(50);
+            GUILayout.Space(20);
             GUILayout.Label("Current Speed: " + thisVessel.verticalSpeed.ToString("N3") + "m/s", GUILayout.Width(250));
             GUILayout.EndHorizontal();
 
@@ -161,43 +229,43 @@ namespace PilotAid
                 GUILayout.BeginHorizontal();
 
                 GUILayout.Label("Altitude Kp: ", GUILayout.Width(80));
-                text = GUILayout.TextField(AltitudeToClimbRate.PGain.ToString(), GUILayout.Width(40));
+                text = GUILayout.TextField(AltitudeToClimbRate.PGain.ToString(), GUILayout.Width(60));
                 AltitudeToClimbRate.PGain = double.Parse(text);
 
                 GUILayout.Space(20);
 
                 GUILayout.Label("Altitude Ki: ", GUILayout.Width(80));
-                text = GUILayout.TextField(AltitudeToClimbRate.IGain.ToString(), GUILayout.Width(40));
+                text = GUILayout.TextField(AltitudeToClimbRate.IGain.ToString(), GUILayout.Width(60));
                 AltitudeToClimbRate.IGain = double.Parse(text);
 
                 GUILayout.Space(20);
 
                 GUILayout.Label("Altitude Kd: ", GUILayout.Width(80));
-                text = GUILayout.TextField(AltitudeToClimbRate.DGain.ToString(), GUILayout.Width(40));
+                text = GUILayout.TextField(AltitudeToClimbRate.DGain.ToString(), GUILayout.Width(60));
                 AltitudeToClimbRate.DGain = double.Parse(text);
 
-                GUILayout.Space(20);
+                GUILayout.Space(5);
 
-                GUILayout.Label("Altitude Min Out: ", GUILayout.Width(80));
-                text = GUILayout.TextField(AltitudeToClimbRate.OutMin.ToString(), GUILayout.Width(40));
+                GUILayout.Label("Altitude Min Out: ", GUILayout.Width(120));
+                text = GUILayout.TextField(AltitudeToClimbRate.OutMin.ToString(), GUILayout.Width(60));
                 AltitudeToClimbRate.OutMin = double.Parse(text);
 
-                GUILayout.Space(20);
+                GUILayout.Space(5);
 
-                GUILayout.Label("Altitude Max Out: ", GUILayout.Width(80));
-                text = GUILayout.TextField(AltitudeToClimbRate.OutMax.ToString(), GUILayout.Width(40));
+                GUILayout.Label("Altitude Max Out: ", GUILayout.Width(120));
+                text = GUILayout.TextField(AltitudeToClimbRate.OutMax.ToString(), GUILayout.Width(60));
                 AltitudeToClimbRate.OutMax = double.Parse(text);
 
-                GUILayout.Space(20);
+                GUILayout.Space(5);
 
-                GUILayout.Label("Clamp Min: ", GUILayout.Width(80));
-                text = GUILayout.TextField(AltitudeToClimbRate.ClampLower.ToString(), GUILayout.Width(40));
+                GUILayout.Label("Clamp Min: ", GUILayout.Width(120));
+                text = GUILayout.TextField(AltitudeToClimbRate.ClampLower.ToString(), GUILayout.Width(60));
                 AltitudeToClimbRate.ClampLower = double.Parse(text);
 
-                GUILayout.Space(20);
+                GUILayout.Space(5);
 
-                GUILayout.Label("Clamp Max: ", GUILayout.Width(80));
-                text = GUILayout.TextField(AltitudeToClimbRate.ClampUpper.ToString(), GUILayout.Width(40));
+                GUILayout.Label("Clamp Max: ", GUILayout.Width(120));
+                text = GUILayout.TextField(AltitudeToClimbRate.ClampUpper.ToString(), GUILayout.Width(60));
                 AltitudeToClimbRate.ClampUpper = double.Parse(text);
 
                 GUILayout.EndHorizontal();
@@ -205,43 +273,43 @@ namespace PilotAid
             GUILayout.BeginHorizontal();
 
             GUILayout.Label("Pitch Kp: ", GUILayout.Width(80));
-            text = GUILayout.TextField(PitchController.PGain.ToString());
+            text = GUILayout.TextField(PitchController.PGain.ToString(), GUILayout.Width(60));
             PitchController.PGain = double.Parse(text);
 
             GUILayout.Space(20);
 
             GUILayout.Label("Pitch Ki: ", GUILayout.Width(80));
-            text = GUILayout.TextField(PitchController.IGain.ToString());
+            text = GUILayout.TextField(PitchController.IGain.ToString(), GUILayout.Width(60));
             PitchController.IGain = double.Parse(text);
 
             GUILayout.Space(20);
 
             GUILayout.Label("Pitch Kd: ", GUILayout.Width(80));
-            text = GUILayout.TextField(PitchController.DGain.ToString());
+            text = GUILayout.TextField(PitchController.DGain.ToString(), GUILayout.Width(60));
             PitchController.DGain = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Pitch Min Out: ", GUILayout.Width(80));
-            text = GUILayout.TextField(PitchController.OutMin.ToString());
+            GUILayout.Label("Pitch Min Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(PitchController.OutMin.ToString(), GUILayout.Width(60));
             PitchController.OutMin = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Pitch Max Out: ", GUILayout.Width(80));
-            text = GUILayout.TextField(PitchController.OutMax.ToString());
+            GUILayout.Label("Pitch Max Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(PitchController.OutMax.ToString(), GUILayout.Width(60));
             PitchController.OutMax = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Pitch Clamp Min: ", GUILayout.Width(80));
-            text = GUILayout.TextField(PitchController.ClampLower.ToString());
+            GUILayout.Label("Pitch Clamp Min: ", GUILayout.Width(120));
+            text = GUILayout.TextField(PitchController.ClampLower.ToString(), GUILayout.Width(60));
             PitchController.ClampLower = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Pitch Clamp Max: ", GUILayout.Width(80));
-            text = GUILayout.TextField(PitchController.ClampUpper.ToString());
+            GUILayout.Label("Pitch Clamp Max: ", GUILayout.Width(120));
+            text = GUILayout.TextField(PitchController.ClampUpper.ToString(), GUILayout.Width(60));
             PitchController.ClampUpper = double.Parse(text);
 
             GUILayout.EndHorizontal();
@@ -249,52 +317,52 @@ namespace PilotAid
             GUILayout.BeginHorizontal();
 
             GUILayout.Label("Elevator Kp: ", GUILayout.Width(80));
-            text = GUILayout.TextField(ElevatorController.PGain.ToString());
+            text = GUILayout.TextField(ElevatorController.PGain.ToString(), GUILayout.Width(60));
             ElevatorController.PGain = double.Parse(text);
 
             GUILayout.Space(20);
 
             GUILayout.Label("Elevator Ki: ", GUILayout.Width(80));
-            text = GUILayout.TextField(ElevatorController.IGain.ToString());
+            text = GUILayout.TextField(ElevatorController.IGain.ToString(), GUILayout.Width(60));
             ElevatorController.IGain = double.Parse(text);
 
             GUILayout.Space(20);
 
             GUILayout.Label("Elevator Kd: ", GUILayout.Width(80));
-            text = GUILayout.TextField(ElevatorController.DGain.ToString());
+            text = GUILayout.TextField(ElevatorController.DGain.ToString(), GUILayout.Width(60));
             ElevatorController.DGain = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Elevator Min Out: ", GUILayout.Width(80));
-            text = GUILayout.TextField(ElevatorController.OutMin.ToString());
+            GUILayout.Label("Elevator Min Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(ElevatorController.OutMin.ToString(), GUILayout.Width(60));
             ElevatorController.OutMin = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Elevator Max Out: ", GUILayout.Width(80));
-            text = GUILayout.TextField(ElevatorController.OutMax.ToString());
+            GUILayout.Label("Elevator Max Out: ", GUILayout.Width(120));
+            text = GUILayout.TextField(ElevatorController.OutMax.ToString(), GUILayout.Width(60));
             ElevatorController.OutMax = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Elevator Clamp Min: ", GUILayout.Width(80));
-            text = GUILayout.TextField(ElevatorController.ClampLower.ToString());
+            GUILayout.Label("Elevator Clamp Min: ", GUILayout.Width(120));
+            text = GUILayout.TextField(ElevatorController.ClampLower.ToString(), GUILayout.Width(60));
             ElevatorController.ClampLower = double.Parse(text);
 
-            GUILayout.Space(20);
+            GUILayout.Space(5);
 
-            GUILayout.Label("Elevator Clamp Max ", GUILayout.Width(80));
-            text = GUILayout.TextField(ElevatorController.ClampUpper.ToString());
+            GUILayout.Label("Elevator Clamp Max ", GUILayout.Width(120));
+            text = GUILayout.TextField(ElevatorController.ClampUpper.ToString(), GUILayout.Width(60));
             ElevatorController.ClampUpper = double.Parse(text);
 
             GUILayout.EndHorizontal();
 
-            pitchActive = GUILayout.Toggle(pitchActive, "Active");
+            pitchActive = GUILayout.Toggle(pitchActive, "Active", GUILayout.Width(200));
             if (pitchActive && !pitchWasActive)
             {
                 if (bAltitudeHold)
-                    targetAltitude = thisVessel.altitude.ToString();
+                    targetAltitude = thisVessel.altitude.ToString("N1");
                 AltitudeToClimbRate.SetPoint = thisVessel.altitude;
                 pitchWasActive = true;
             }
@@ -320,7 +388,11 @@ namespace PilotAid
             
             // Wing leveller
             if (rollActive)
+            {
+                RollController.SetPoint = HeadingController.Response(heading);
+
                 state.roll = (float)PID.PID_Controller.Clamp(RollController.Response(roll) + state.roll, -1, 1);
+            }
 
             // Pitch Controller
             // Work on vertical speed, altitude hold can use a proportional error as input
