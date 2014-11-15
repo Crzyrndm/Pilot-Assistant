0603b40e37fab9d73b14a602c6ff4c032e7638cb
CrzyRndm <joshcrawfy@gmail.com>
2014-11-16 12:24:01 +1300

    Yaw integrated into Hdg

--- PilotAssistant/PilotAssistant.cs
+++ PilotAssistant/PilotAssistant.cs
@@ -8,15 +8,15 @@ namespace PilotAssistant
     {        
         private Vessel thisVessel = null;
         private PID.PID_Controller HeadingController = new PID.PID_Controller(3, 0.1, 0, -30, 30, -0.1, 0.1);
+        private PID.PID_Controller HeadingYawController = new PID.PID_Controller(0, 0, 0, -2, 2, -2, 2);
         private PID.PID_Controller AileronController = new PID.PID_Controller(0.01, 0.01, 0.01, -1, 1, -0.1, 0.1);
+        private PID.PID_Controller RudderController = new PID.PID_Controller(0.05, 0.01, 0.1, -1, 1, -0.1, 0.1);
 
         private PID.PID_Controller AltitudeToClimbRate = new PID.PID_Controller(0.1, 0, 0, -30, 30, -1, 1); // P control for converting altitude hold to climb rate
         private PID.PID_Controller AoAController = new PID.PID_Controller(3, 0.4, 1.5, -10, 10, -10, 10); // Input craft altitude, output target craft AoA
         private PID.PID_Controller ElevatorController = new PID.PID_Controller(0.01, 0.01, 0.01, -1, 1, -0.1, 0.1); // Convert pitch input to control surface deflection
 
-        private PID.PID_Controller YawController = new PID.PID_Controller(0.05, 0.01, 0.1, -1, 1, -0.1, 0.1);
-
-        private Rect window = new Rect(10, 50, 500, 490);
+        private Rect window = new Rect(10, 50, 500, 380);
         // RollController
         private bool rollActive = false;
         private bool rollWasActive = false;
@@ -29,25 +29,39 @@ namespace PilotAssistant
 
         private double pitch = 0, roll = 0, yaw = 0, AoA = 0, heading = 0; // currenct craft attitude variables
 
-        private string targetAltitude = "0";
-        private string targetSpeed = "0";
+        private string targetVert = "0";
         private string targetHeading = "0";
 
         private bool showPIDGains = false;
         private bool showPIDLimits = false;
         private bool showControlSurfaces = false;
 
-        private Vector2 scrollbar = Vector2.zero;
+        private Vector2 scrollbarHdg = Vector2.zero;
+        private Vector2 scrollbarVert = Vector2.zero;
 
         public void Start()
         {
             thisVessel = FlightGlobals.ActiveVessel;
             thisVessel.OnFlyByWire += new FlightInputCallback(vesselController);
+            GameEvents.onVesselChange.Add(vesselSwitch);
 
             AileronController.InMax = 180;
             AileronController.InMin = -180;
 
             AltitudeToClimbRate.InMin = 0;
+        }
+
+        public void vesselSwitch(Vessel v)
+        {
+            thisVessel.OnFlyByWire -= new FlightInputCallback(vesselController);
+            thisVessel = v;
+            thisVessel.OnFlyByWire += new FlightInputCallback(vesselController);
+        }
+
+        public void OnDestroy()
+        {
+            appDestroy();
+            GameEvents.onVesselChange.Remove(vesselSwitch);
         }
 
         public void Update()
@@ -70,12 +84,12 @@ namespace PilotAssistant
                 pitchWasActive = true;
                 if (bAltitudeHold)
                 {
-                    targetAltitude = thisVessel.altitude.ToString("N1");
+                    targetVert = thisVessel.altitude.ToString("N1");
                     AltitudeToClimbRate.SetPoint = thisVessel.altitude;
                 }
                 else
                 {
-                    targetSpeed = thisVessel.verticalSpeed.ToString("N3");
+                    targetVert = thisVessel.verticalSpeed.ToString("N3");
                     AoAController.SetPoint = thisVessel.verticalSpeed;
                 }
             }
@@ -93,16 +107,21 @@ namespace PilotAssistant
                 if (bAltitudeHold)
                 {
                     AltitudeToClimbRate.SetPoint = thisVessel.altitude;
-                    targetAltitude = AltitudeToClimbRate.SetPoint.ToString("N1");
+                    targetVert = AltitudeToClimbRate.SetPoint.ToString("N1");
                 }
                 else
                 {
                     AoAController.SetPoint = thisVessel.verticalSpeed;
-                    targetSpeed = AoAController.SetPoint.ToString("N3");
+                    targetVert = AoAController.SetPoint.ToString("N3");
                 }
             }
 
-            if (showPIDLimits)
+            if (showPIDGains)
+                window.height = 700;
+            else
+                window.height = 390;
+
+            if (showPIDLimits && showPIDGains)
                 window.width = 390;
             else
                 window.width = 245;
@@ -128,14 +147,17 @@ namespace PilotAssistant
                 showPIDLimits = GUILayout.Toggle(showPIDLimits, "Show PID Limits", GUILayout.Width(200));
                 showControlSurfaces = GUILayout.Toggle(showControlSurfaces, "Show Control Surfaces", GUILayout.Width(200));
             }
-            
-            scrollbar = GUILayout.BeginScrollView(scrollbar);
+            GUILayout.Box("", GUILayout.Height(10), GUILayout.Width(window.width - 50));
+
             GUILayout.BeginVertical();
 
             #region Hdg GUI
             GUILayout.Label("Heading Control", GUILayout.Width(100));
 
-            rollActive = GUILayout.Toggle(rollActive, "Heading Lock Active", GUILayout.Width(200));
+            if(GUILayout.Button(rollActive ? "Deactivate" : "Activate", GUILayout.Width(200)))
+            {
+                rollActive = !rollActive;
+            }
 
             GUILayout.BeginHorizontal();
             GUILayout.Label("Target Heading: ", GUILayout.Width(98));
@@ -153,45 +175,49 @@ namespace PilotAssistant
                 }
             }
 
-            drawPIDvalues(HeadingController, "Hdg", "\u00B0", heading, 2, "Roll", "\u00B0");
-            drawPIDvalues(AileronController, "Bank", "\u00B0", roll, 3, "Deflection", "\u00B0");
+            scrollbarHdg = GUILayout.BeginScrollView(scrollbarHdg, showPIDGains ? GUILayout.Height(160) : GUILayout.Height(0));
 
+            drawPIDvalues(HeadingController, "Hdg Roll", "\u00B0", heading, 2, "Bank", "\u00B0");
+            if (showControlSurfaces)
+                drawPIDvalues(AileronController, "Bank", "\u00B0", roll, 3, "Deflection", "\u00B0");
+            drawPIDvalues(HeadingYawController, "Hdg Yaw", "\u00B0", heading, 2, "Yaw", "\u00B0", false, false);
+            if (showControlSurfaces)
+                drawPIDvalues(RudderController, "Yaw", "\u00B0", yaw, 3, "Deflection", "\u00B0");
+
+            GUILayout.EndScrollView();
             #endregion
 
-            #region Pitch GUI
-            GUILayout.Label("", GUILayout.Height(10));
+            GUILayout.Box("", GUILayout.Height(10), GUILayout.Width(window.width - 50));
 
-            //// Pitch Controls
+            #region Pitch GUI
+
             GUILayout.Label("Vertical Control");
-            pitchActive = GUILayout.Toggle(pitchActive, "Pitch Control Active", GUILayout.Width(200));
+            
+            if(GUILayout.Button(pitchActive ? "Deactivate" : "Activate", GUILayout.Width(200)))
+            {
+                pitchActive = !pitchActive;
+            }
+    
             bAltitudeHold = GUILayout.Toggle(bAltitudeHold, bAltitudeHold ? "Mode: Altitude" : "Mode: Vertical Speed", GUILayout.Width(200));
 
             GUILayout.BeginHorizontal();
             GUILayout.Label(bAltitudeHold ? "Target Altitude: " : "Target Speed: ", GUILayout.Width(98));
-            if (bAltitudeHold)
-                targetAltitude = GUILayout.TextField(targetAltitude, GUILayout.Width(98));
-            else
-                targetSpeed = GUILayout.TextField(targetSpeed, GUILayout.Width(98));
+            targetVert = GUILayout.TextField(targetVert, GUILayout.Width(98));
             GUILayout.EndHorizontal();
 
             if (GUILayout.Button(bAltitudeHold ? "Update Target Altitude" : "Update Target Speed", GUILayout.Width(200)))
             {
                 pitchActive = true;
+
+                double newVal;
+                double.TryParse(targetVert, out newVal);
                 if (bAltitudeHold)
-                {
-                    double newAlt;
-                    double.TryParse(targetAltitude, out newAlt);
-                    AltitudeToClimbRate.SetPoint = newAlt;
-                }
+                    AltitudeToClimbRate.SetPoint = newVal;
                 else
-                {
-                    double newSpeed;
-                    double.TryParse(targetSpeed, out newSpeed);
-                    AoAController.SetPoint = newSpeed;
-                }
+                    AoAController.SetPoint = newVal;
             }
 
-
+            scrollbarVert = GUILayout.BeginScrollView(scrollbarVert);
             if (bAltitudeHold)
             {
                 drawPIDvalues(AltitudeToClimbRate, "Alt", "m", thisVessel.altitude, 1, "Speed ", "m/s", true);
@@ -203,35 +229,31 @@ namespace PilotAssistant
             }
             #endregion
 
-            #region Yaw damper
-            GUILayout.Label("", GUILayout.Height(10));
-            GUILayout.Label("Yaw Damper", GUILayout.Width(150));
-
-            drawPIDvalues(YawController, "Yaw", "\u00B0", yaw, 3, "Deflection", "\u00B0");
-
-            bool yawActive = GUILayout.Toggle(pitchActive || rollActive, "Yaw Damper Active", GUILayout.Width(200));
-            #endregion
-
             GUILayout.EndVertical();
             GUILayout.EndScrollView();
 
             GUI.DragWindow();
         }
 
-        private void drawPIDvalues(PID.PID_Controller controller, string inputName, string inputUnits, double inputValue, int displayPrecision, string outputName, string outputUnits, bool invertOutput = false)
+        private void drawPIDvalues(PID.PID_Controller controller, string inputName, string inputUnits, double inputValue, int displayPrecision, string outputName, string outputUnits, bool invertOutput = false, bool showCurrent = true)
         {
-            GUILayout.Label(string.Format("Current {0}: ", inputName) + inputValue.ToString("N" + displayPrecision.ToString()) + inputUnits, GUILayout.Width(200));
+            if (showPIDGains)
+                GUILayout.Box("", GUILayout.Height(5), GUILayout.Width(window.width - 50));
+
+            if (showCurrent)
+                GUILayout.Label(string.Format("Current {0}: ", inputName) + inputValue.ToString("N" + displayPrecision.ToString()) + inputUnits, GUILayout.Width(200));
             
             if (showPIDGains)
             {
-                GUILayout.Label(string.Format("Target {0}: ", inputName) + controller.SetPoint.ToString("N" + displayPrecision.ToString()) + inputUnits, GUILayout.Width(200));
+                if (showCurrent)
+                    GUILayout.Label(string.Format("Target {0}: ", inputName) + controller.SetPoint.ToString("N" + displayPrecision.ToString()) + inputUnits, GUILayout.Width(200));
 
                 GUILayout.BeginHorizontal();
                 GUILayout.BeginVertical();
                 
-                controller.PGain = labPlusBox(string.Format("{0} Kp: ", inputName), controller.PGain.ToString(), 80);
-                controller.IGain = labPlusBox(string.Format("{0} Ki: ", inputName), controller.IGain.ToString(), 80);
-                controller.DGain = labPlusBox(string.Format("{0} Kd: ", inputName), controller.DGain.ToString(), 80);
+                controller.PGain = labPlusNumBox(string.Format("{0} Kp: ", inputName), controller.PGain.ToString(), 80);
+                controller.IGain = labPlusNumBox(string.Format("{0} Ki: ", inputName), controller.IGain.ToString(), 80);
+                controller.DGain = labPlusNumBox(string.Format("{0} Kd: ", inputName), controller.DGain.ToString(), 80);
 
                 if (showPIDLimits)
                 {
@@ -240,33 +262,38 @@ namespace PilotAssistant
 
                     if (!invertOutput)
                     {
-                        controller.OutMin = labPlusBox(string.Format("Min {0}{1}: ", outputName, outputUnits), controller.OutMin.ToString());
-                        controller.OutMax = labPlusBox(string.Format("Max {0}{1}: ", outputName, outputUnits), controller.OutMax.ToString());
-                        controller.ClampLower = labPlusBox("I Clamp Lower", controller.ClampLower.ToString());
-                        controller.ClampUpper = labPlusBox("I Clamp Upper", controller.ClampUpper.ToString());
+                        controller.OutMin = labPlusNumBox(string.Format("Min {0}{1}: ", outputName, outputUnits), controller.OutMin.ToString());
+                        controller.OutMax = labPlusNumBox(string.Format("Max {0}{1}: ", outputName, outputUnits), controller.OutMax.ToString());
+                        controller.ClampLower = labPlusNumBox("I Clamp Lower", controller.ClampLower.ToString());
+                        controller.ClampUpper = labPlusNumBox("I Clamp Upper", controller.ClampUpper.ToString());
                     }
                     else
-                    {
-                        controller.OutMax = -1 * labPlusBox(string.Format("Min {0}{1}: ", outputName, outputUnits), (-controller.OutMax).ToString());
-                        controller.OutMin = -1 * labPlusBox(string.Format("Max {0}{1}: ", outputName, outputUnits), (-controller.OutMin).ToString());
-                        controller.ClampUpper = -1 * labPlusBox("I Clamp Lower", (-controller.ClampUpper).ToString());
-                        controller.ClampLower = -1 * labPlusBox("I Clamp Upper", (-controller.ClampLower).ToString());
+                    { // used when response * -1 is used to get the correct output
+                        controller.OutMax = -1 * labPlusNumBox(string.Format("Min {0}{1}: ", outputName, outputUnits), (-controller.OutMax).ToString());
+                        controller.OutMin = -1 * labPlusNumBox(string.Format("Max {0}{1}: ", outputName, outputUnits), (-controller.OutMin).ToString());
+                        controller.ClampUpper = -1 * labPlusNumBox("I Clamp Lower", (-controller.ClampUpper).ToString());
+                        controller.ClampLower = -1 * labPlusNumBox("I Clamp Upper", (-controller.ClampLower).ToString());
                     }
                 }
                 GUILayout.EndVertical();
                 GUILayout.EndHorizontal();
             }
-            GUILayout.Box("", GUILayout.Height(5), GUILayout.Width(window.width - 50));
         }
 
-        private double labPlusBox(string labelText, string boxText, float labelWidth = 120, float boxWidth = 60)
+        private double labPlusNumBox(string labelText, string boxText, float labelWidth = 120, float boxWidth = 60)
         {
             GUILayout.BeginHorizontal();
             GUILayout.Label(labelText, GUILayout.Width(labelWidth));
             string text = GUILayout.TextField(boxText, GUILayout.Width(boxWidth));
             GUILayout.EndHorizontal();
-
-            return double.Parse(text);
+            try
+            {
+                return double.Parse(text);
+            }
+            catch
+            {
+                return double.Parse(boxText);
+            }
         }
 
         private void vesselController(FlightCtrlState state)
@@ -283,13 +310,23 @@ namespace PilotAssistant
             {
                 // Fix heading so it behaves properly traversing 0/360 degrees
                 if (HeadingController.SetPoint - heading >= -180 && HeadingController.SetPoint - heading <= 180)
+                {
                     AileronController.SetPoint = HeadingController.Response(heading);
+                    RudderController.SetPoint = HeadingYawController.Response(heading);
+                }
                 else if (HeadingController.SetPoint - heading < -180)
+                {
                     AileronController.SetPoint = HeadingController.Response(heading - 360);
+                    RudderController.SetPoint = HeadingYawController.Response(heading - 360);
+                }
                 else if (HeadingController.SetPoint - heading > 180)
+                {
                     AileronController.SetPoint = HeadingController.Response(heading + 360);
+                    RudderController.SetPoint = HeadingYawController.Response(heading + 360);
+                }
 
                 state.roll = (float)Clamp(AileronController.Response(roll) + state.roll, -1, 1);
+                state.yaw = (float)Clamp(RudderController.Response(yaw) + state.yaw, -1, 1);
             }
 
             // Pitch Controller
@@ -302,12 +339,6 @@ namespace PilotAssistant
 
                 ElevatorController.SetPoint = -AoAController.Response(thisVessel.verticalSpeed);
                 state.pitch = (float)Clamp(-ElevatorController.Response(AoA) + state.pitch, -1, 1);
-            }
-
-            if (rollActive || pitchActive)
-            {
-                print("Yaw: " + yaw.ToString());
-                state.yaw = (float)Clamp(YawController.Response(yaw) + state.yaw, -1, 1);
             }
         }
 
@@ -349,7 +380,7 @@ namespace PilotAssistant
             GameEvents.onGUIApplicationLauncherReady.Add(this.OnAppLauncherReady);
         }
 
-        private void OnDestroy()
+        private void appDestroy()
         {
             GameEvents.onGUIApplicationLauncherReady.Remove(this.OnAppLauncherReady);
             if (btnLauncher != null)
