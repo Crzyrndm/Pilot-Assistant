01d5708c99bdb41cda68968eb8af2b9f2274beeb
CrzyRndm <joshcrawfy@gmail.com>
2014-11-24 17:44:38 +1300

    AtmoSAS

--- PilotAssistant/AtmoSAS.cs
+++ PilotAssistant/AtmoSAS.cs
@@ -12,7 +12,7 @@ namespace PilotAssistant
     internal enum SASList
     {
         Pitch,
-        Yaw,
+        Hdg,
         Roll
     }
 
@@ -27,7 +27,7 @@ namespace PilotAssistant
         internal bool[] bPause = new bool[3]; // pause on a per axis basis
         internal bool bAtmosphere = false;
 
-        internal Rect SASwindow = new Rect(350, 50, 245, 30);
+        internal Rect SASwindow = new Rect(350, 50, 200, 30);
 
         internal Vector2 scroll = new Vector2(0, 0);
 
@@ -35,6 +35,10 @@ namespace PilotAssistant
         internal GUIStyle textStyle;
         internal GUIStyle btnStyle1;
         internal GUIStyle btnStyle2;
+
+        internal Quaternion target = new Quaternion(0, 0, 0, 0);
+
+        internal bool bStockSAS = false;
 
         public void Initialise()
         {
@@ -46,15 +50,15 @@ namespace PilotAssistant
             if (FlightData.thisVessel.VesselSAS.pidLockedPitch != null)
             {
                 PIDclamp c = FlightData.thisVessel.VesselSAS.pidLockedPitch;
-                PID_Controller pitch = new PID.PID_Controller(0.15, 0.0, 0.06, -1, 1, -0.2, 0.2, 5);
+                PID_Controller pitch = new PID.PID_Controller(0.15, 0.0, 0.06, -1, 1, -0.2, 0.2, 1);
                 SASControllers.Add(pitch);
 
                 c = FlightData.thisVessel.VesselSAS.pidLockedYaw;
-                PID_Controller yaw = new PID.PID_Controller(0.15, 0.0, 0.06, -1, 1, -0.2, 0.2, 5);
+                PID_Controller yaw = new PID.PID_Controller(0.15, 0.0, 0.06, -1, 1, -0.2, 0.2, 1);
                 SASControllers.Add(yaw);
 
                 c = FlightData.thisVessel.VesselSAS.pidLockedRoll;
-                PID_Controller roll = new PID.PID_Controller(0.1, 0.0, 0.06, -1, 1, -0.2, 0.2, 5);
+                PID_Controller roll = new PID.PID_Controller(0.1, 0.0, 0.06, -1, 1, -0.2, 0.2, 3);
                 SASControllers.Add(roll);
 
                 bInit = true;
@@ -86,7 +90,12 @@ namespace PilotAssistant
             else
                 bAtmosphere = false;
 
-            pauseManager();
+            if (bStockSAS)
+                SASwindow.height = 420;
+            else
+                SASwindow.height = 530;
+
+            pauseManager(); // manage activation of SAS axes depending on user input
         }
 
         public void OnGUI()
@@ -107,65 +116,83 @@ namespace PilotAssistant
 
             if (AppLauncherInstance.bDisplaySAS)
             {
-                SASwindow = GUILayout.Window(78934856, SASwindow, drawSASWindow, "");
+                SASwindow = GUI.Window(78934856, SASwindow, drawSASWindow, "");
             }
-            if (bActive)
+            if (bArmed && bActive)
                 GUI.Box(new Rect(Screen.width / 2 + 100, Screen.height - 200, 55, 30), "Active");
         }
 
         public void FixedUpdate()
         {
-            if (bInit && bActive)
+            if (bInit && bArmed && bActive)
             {
                 FlightData.updateAttitude();
 
                 float pitchResponse = -1 * (float)SASControllers[(int)SASList.Pitch].Response(FlightData.pitch);
-                float yawResponse = -1 * (float)SASControllers[(int)SASList.Yaw].Response(FlightData.heading);
+                float yawResponse = -1 * (float)SASControllers[(int)SASList.Hdg].Response(FlightData.heading);
                 double rollRad = Math.PI / 180 * FlightData.roll;
 
                 if (!bPause[(int)SASList.Pitch])
-                    FlightData.thisVessel.ctrlState.pitch = pitchResponse * (float)Math.Cos(rollRad) + yawResponse * (float)Math.Sin(rollRad);
+                    FlightData.thisVessel.ctrlState.pitch = pitchResponse * (float)Math.Cos(rollRad) - yawResponse * (float)Math.Sin(rollRad);
 
                 if (!bPause[(int)SASList.Roll])
                     FlightData.thisVessel.ctrlState.roll = (float)SASControllers[(int)SASList.Roll].Response(FlightData.roll);
 
-                if (!bPause[(int)SASList.Yaw])
+                if (!bPause[(int)SASList.Hdg])
                     FlightData.thisVessel.ctrlState.yaw = pitchResponse * (float)Math.Sin(rollRad) + yawResponse * (float)Math.Cos(rollRad);
             }
         }
 
         private void updateTarget()
         {
-            FlightData.updateAttitude();
+            target = FlightData.surfAtt();
+            
             SASControllers[(int)SASList.Pitch].SetPoint = FlightData.pitch;
+            SASControllers[(int)SASList.Hdg].SetPoint = FlightData.heading;
             SASControllers[(int)SASList.Roll].SetPoint = FlightData.roll;
-            SASControllers[(int)SASList.Yaw].SetPoint = FlightData.heading;
         }
 
         private void drawSASWindow(int id)
         {
-            if (GUILayout.Button(bArmed ? "Disarm SAS" : "Arm SAS"))
-                bArmed = !bArmed;
-            //GUILayout.Label("Atmospheric Mode: " + bAtmosphere.ToString());
+            bStockSAS = GUILayout.Toggle(bStockSAS, "Use Stock SAS");
+            if (!bStockSAS)
+            {
+                if (GUILayout.Button(bArmed ? "Disarm SAS" : "Arm SAS"))
+                    bArmed = !bArmed;
+                //GUILayout.Label("Atmospheric Mode: " + bAtmosphere.ToString());
 
-            scroll = GUILayout.BeginScrollView(scroll, GUILayout.Height(MainWindow.window.height - 100), GUILayout.MinHeight(150));
+                SASControllers[(int)SASList.Pitch].SetPoint = (float)labPlusNumBox2("Pitch:", SASControllers[(int)SASList.Pitch].SetPoint.ToString("N2"), 80);
+                SASControllers[(int)SASList.Hdg].SetPoint = (float)labPlusNumBox2("Heading:", SASControllers[(int)SASList.Hdg].SetPoint.ToString("N2"), 80);
+                SASControllers[(int)SASList.Roll].SetPoint = (float)labPlusNumBox2("Roll:", SASControllers[(int)SASList.Roll].SetPoint.ToString("N2"), 80);
 
-            drawPIDvalues(SASControllers[(int)SASList.Pitch], "Pitch");
-            drawPIDvalues(SASControllers[(int)SASList.Roll], "Roll");
-            drawPIDvalues(SASControllers[(int)SASList.Yaw], "Yaw");
+                drawPIDvalues(SASControllers[(int)SASList.Pitch], "Pitch");
+                drawPIDvalues(SASControllers[(int)SASList.Roll], "Roll");
+                drawPIDvalues(SASControllers[(int)SASList.Hdg], "Yaw");
+            }
+            else
+            {
+                VesselSAS sas = FlightData.thisVessel.VesselSAS;
 
-            GUILayout.EndScrollView();
+                drawStockPIDvalues(sas.pidLockedPitch, "Pitch");
+                drawStockPIDvalues(sas.pidLockedRoll, "Roll");
+                drawStockPIDvalues(sas.pidLockedYaw, "Yaw");
+            }
             GUI.DragWindow();
         }
 
         private void pauseManager()
         {
-            if (GameSettings.PITCH_DOWN.GetKeyDown() || GameSettings.PITCH_UP.GetKeyDown())
+            if (GameSettings.PITCH_DOWN.GetKeyDown() || GameSettings.PITCH_UP.GetKeyDown() || GameSettings.YAW_LEFT.GetKeyDown() || GameSettings.YAW_RIGHT.GetKeyDown())
+            {
                 bPause[(int)SASList.Pitch] = true;
-            if (GameSettings.PITCH_DOWN.GetKeyUp() || GameSettings.PITCH_UP.GetKeyUp())
+                bPause[(int)SASList.Hdg] = true;
+            }
+            if (GameSettings.PITCH_DOWN.GetKeyUp() || GameSettings.PITCH_UP.GetKeyUp() || GameSettings.YAW_LEFT.GetKeyUp() || GameSettings.YAW_RIGHT.GetKeyUp())
             {
                 bPause[(int)SASList.Pitch] = false;
+                bPause[(int)SASList.Hdg] = false;
                 SASControllers[(int)SASList.Pitch].SetPoint = FlightData.pitch;
+                SASControllers[(int)SASList.Hdg].SetPoint = FlightData.heading;
             }
 
             if (GameSettings.ROLL_LEFT.GetKeyDown() || GameSettings.ROLL_RIGHT.GetKeyDown())
@@ -176,22 +203,22 @@ namespace PilotAssistant
                 SASControllers[(int)SASList.Roll].SetPoint = FlightData.roll;
             }
 
-            if (GameSettings.YAW_LEFT.GetKeyDown() || GameSettings.YAW_RIGHT.GetKeyDown())
-                bPause[(int)SASList.Yaw] = true;
-            if (GameSettings.YAW_LEFT.GetKeyUp() || GameSettings.YAW_RIGHT.GetKeyUp())
-            {
-                bPause[(int)SASList.Yaw] = false;
-                SASControllers[(int)SASList.Yaw].SetPoint = FlightData.heading;
-            }
+            //if (GameSettings.YAW_LEFT.GetKeyDown() || GameSettings.YAW_RIGHT.GetKeyDown())
+            //    bPause[(int)SASList.Yaw] = true;
+            //if (GameSettings.YAW_LEFT.GetKeyUp() || GameSettings.YAW_RIGHT.GetKeyUp())
+            //{
+            //    bPause[(int)SASList.Yaw] = false;
+            //    SASControllers[(int)SASList.Yaw].SetPoint = FlightData.heading;
+            //}
 
             if (GameSettings.SAS_HOLD.GetKeyDown())
             {
-                bPause[(int)SASList.Pitch] = bPause[(int)SASList.Roll] = bPause[(int)SASList.Yaw] = true;
+                bPause[(int)SASList.Pitch] = bPause[(int)SASList.Roll] = bPause[(int)SASList.Hdg] = true;
                 FlightData.thisVessel.ActionGroups.SetGroup(KSPActionGroup.SAS, false);
             }
             if (GameSettings.SAS_HOLD.GetKeyUp())
             {
-                bPause[(int)SASList.Pitch] = bPause[(int)SASList.Roll] = bPause[(int)SASList.Yaw] = false;
+                bPause[(int)SASList.Pitch] = bPause[(int)SASList.Roll] = bPause[(int)SASList.Hdg] = false;
                 updateTarget();
                 FlightData.thisVessel.ActionGroups.SetGroup(KSPActionGroup.SAS, false);
             }
@@ -205,6 +232,16 @@ namespace PilotAssistant
             controller.IGain = labPlusNumBox(string.Format("{0} Ki: ", inputName), controller.IGain.ToString("G3"), 80);
             controller.DGain = labPlusNumBox(string.Format("{0} Kd: ", inputName), controller.DGain.ToString("G3"), 80);
             controller.Scalar = labPlusNumBox(string.Format("{0} Scalar: ", inputName), controller.Scalar.ToString("G3"), 80);
+        }
+
+        private void drawStockPIDvalues(PIDclamp controller, string inputName)
+        {
+            GUILayout.Box("", GUILayout.Height(5), GUILayout.Width(SASwindow.width - 50));
+
+            controller.kp = labPlusNumBox(string.Format("{0} Kp: ", inputName), controller.kp.ToString("G3"), 80);
+            controller.ki = labPlusNumBox(string.Format("{0} Ki: ", inputName), controller.ki.ToString("G3"), 80);
+            controller.kd = labPlusNumBox(string.Format("{0} Kd: ", inputName), controller.kd.ToString("G3"), 80);
+            controller.clamp = labPlusNumBox(string.Format("{0} Scalar: ", inputName), controller.clamp.ToString("G3"), 80);
         }
 
         private double labPlusNumBox(string labelText, string boxText, float labelWidth = 100, float boxWidth = 60)
@@ -237,6 +274,38 @@ namespace PilotAssistant
             if (GUILayout.Button("-", btnStyle2, GUILayout.Width(20), GUILayout.Height(13)))
             {
                 val /= 1.1;
+            }
+            GUILayout.EndVertical();
+            //
+            GUILayout.EndHorizontal();
+            return val;
+        }
+
+        private double labPlusNumBox2(string labelText, string boxText, float labelWidth = 100, float boxWidth = 60)
+        {
+            double val;
+            GUILayout.BeginHorizontal();
+
+            GUILayout.Label(labelText, labelStyle, GUILayout.Width(labelWidth));
+            string text = GUILayout.TextField(boxText, textStyle, GUILayout.Width(boxWidth));
+            //
+            try
+            {
+                val = double.Parse(text);
+            }
+            catch
+            {
+                val = double.Parse(boxText);
+            }
+            //
+            GUILayout.BeginVertical();
+            if (GUILayout.Button("+", btnStyle1, GUILayout.Width(20), GUILayout.Height(13)))
+            {
+                val += 1;
+            }
+            if (GUILayout.Button("-", btnStyle2, GUILayout.Width(20), GUILayout.Height(13)))
+            {
+                val -= 1;
             }
             GUILayout.EndVertical();
             //
--- PilotAssistant/Utility/FlightData.cs
+++ PilotAssistant/Utility/FlightData.cs
@@ -14,6 +14,8 @@ namespace PilotAssistant.Utility
         internal static double AoA = 0;
         internal static double heading = 0;
 
+        internal static Quaternion attitude = Quaternion.identity;
+
         internal static NavBall ball;
         internal static void updateAttitude()
         {
@@ -28,18 +30,38 @@ namespace PilotAssistant.Utility
             heading = vesselRot.eulerAngles.y;
 
             // pitch AoA
-            Vector3 tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + thisVessel.ReferenceTransform.forward * Vector3.Dot(thisVessel.ReferenceTransform.forward, thisVessel.srf_velocity.normalized);   //velocity vector projected onto a plane that divides the airplane into left and right halves
+            Vector3 tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + 
+                thisVessel.ReferenceTransform.forward * Vector3.Dot(thisVessel.ReferenceTransform.forward, thisVessel.srf_velocity.normalized);   //velocity vector projected onto a plane that divides the airplane into left and right halves
             AoA = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.forward);
             AoA = 180 / Math.PI * Math.Asin(AoA);
             if (double.IsNaN(AoA))
                 AoA = 0;
 
             // yaw AoA
-            tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + thisVessel.ReferenceTransform.right * Vector3.Dot(thisVessel.ReferenceTransform.right, thisVessel.srf_velocity.normalized);     //velocity vector projected onto the vehicle-horizontal plane
+            tmpVec = thisVessel.ReferenceTransform.up * Vector3.Dot(thisVessel.ReferenceTransform.up, thisVessel.srf_velocity.normalized) + 
+                thisVessel.ReferenceTransform.right * Vector3.Dot(thisVessel.ReferenceTransform.right, thisVessel.srf_velocity.normalized);     //velocity vector projected onto the vehicle-horizontal plane
             yaw = Vector3.Dot(tmpVec.normalized, thisVessel.ReferenceTransform.right);
             yaw = 180 / Math.PI * Math.Asin(yaw);
             if (double.IsNaN(yaw))
                 yaw = 0;
+
+            attitude = surfAtt();
+        }
+
+        internal static Quaternion surfAtt()
+        {
+            // Construct surface relative forward Vector3
+            // heading is on x-y plane, z is pitch angle
+            // heading 0/360 => x = 1, y = 0
+            // heading 90 => x = 0, y = 1
+            // heading 180 => x = -1, y = 0
+            // heading 270 => x = 0, y = -1
+            // pitch 0 => z = 0
+            Vector3d surf = new Vector3d(0, 0, 0);
+            surf.x = Math.Cos(heading * Math.PI / 180);
+            surf.y = Math.Sin(heading * Math.PI / 180);
+            surf.z = Math.Sin(pitch * Math.PI / 180);
+            return Quaternion.LookRotation(surf);
         }
     }
 }
